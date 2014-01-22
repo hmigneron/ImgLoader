c117340437351ead414a91cf6f9484301d3aedc4
Hugo Migneron <hugo.migneron@gmail.com>
2014-01-22 09:45:13 -0500

    Add initial version of image loader. Still very flaky with lots of work to do.

---
+++ imgloader.js
@@ -0,0 +1,64 @@
+//requires jQuery
+(function(window, $) {
+
+	if(!window.ImgLoader) {
+		window.ImgLoader = {};
+	}
+
+	//TODO : Add options to search only a part of the DOM, 
+	//		 Add option to load images from css (and from pseudo states)
+	//		 Add option to load other images too (that could be loaded rfom js later on for example)
+	var settings = ImgLoader.settings = {
+		itemLoaded : null,
+		itemError : null,
+		itemsCompleted : null
+	};
+
+  	ImgLoader.configure = function(options) {
+    	$.extend(settings, options);
+    	return this;
+  	};
+
+  	ImgLoader.loadImages = function() {
+
+  		var itemsLoadedCount = 0,
+  			itemsCount = 0;
+
+  		var onImageLoad = function() {
+  			itemsLoadedCount++;
+  			if(settings.itemLoaded !== null && typeof settings.itemLoaded === 'function') {
+  				settings.itemLoaded(itemsLoadedCount / itemsCount);
+  			}
+
+  			if(itemsLoadedCount === itemsCount) {
+  				if(settings.itemsCompleted !== null && typeof settings.itemsCompleted === 'function') {
+  					settings.itemsCompleted();
+  				}
+  			}
+  		}
+
+  		var onImageError = function() {
+  			itemsLoadedCount++;
+  			if(settings.itemError !== null && typeof settings.itemError === 'function') {
+  				settings.itemError(itemsLoadedCount / itemsCount);
+  			}
+
+  			if(itemsLoadedCount === itemsCount) {
+  				if(settings.itemsCompleted !== null && typeof settings.itemsCompleted === 'function') {
+  					settings.itemsCompleted();
+  				}
+  			}
+  		};
+
+  		var allImages = $('img[src]');
+  		itemsCount = allImages.length;
+
+  		for(var i=0; i<itemsCount; i++) {
+			$("<img/>")
+    			.load(onImageLoad)
+    			.error(onImageError)
+    			.attr("src", $(allImages[i]).attr("src"));
+		}
+  	};
+
+})(window, jQuery, undefined);
